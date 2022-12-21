2c045a45219c5dc3f25d1f17b0519c330088789e
rblf <rblf@g3c02.cin.ufpe.br>
2022-12-21 13:09:22 -0300

    monitoria - aula 01

---
+++ monitoria/aula01.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define TAM 5
+
+// structs
+
+struct _no { // similar to Type in ts
+    int x;
+    char y;
+    int y;
+};
+
+struct retorno {
+    int valor;
+    int num;
+};
+
+retorno criar() {
+
+    retorno *ponteiro;
+    retorno var;
+
+    ponteiro->valor = 10;
+    ponteiro->num = 30;
+
+    var.num = 30;
+    var.valor = 10;
+
+    return ponteiro; // possible way to return 2+ values in one function
+}
+
+// ** is used to appoint
+void modifica_array(int **arr, int valor) {
+    for(int i=0; i < TAM; i++){
+        *(*(arr) + i * sizeof(int)) = valor; // attributing value to x[i]
+    }
+}
+
+int main() {
+    int *x;
+    int valor = 2;
+    x = (int *) malloc(TAM * sizeof(int));
+
+    modifica_array(&x, valor);
+
+    for (int i = 0; i < TAM; i++) {
+        std::cout << x[i] << std::endl;
+    }
+
+    return 0;
+}
