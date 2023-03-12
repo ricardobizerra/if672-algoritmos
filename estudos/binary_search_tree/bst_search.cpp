4dba7bb4755d28242642aeef4d7a4ab5bfed373c
Ricardo Bizerra <ricardo.bizerra19@gmail.com>
2023-03-12 13:42:07 -0300

    feature(main): binary search tree search algorithm

---
+++ estudos/binary_search_tree/bst_search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int value;
+    node *left;
+    node *right;
+    node(int value): value(value), left(NULL), right(NULL) {}
+};
+
+node* bst_search(node *root, int v) {
+    if (root == NULL) return NULL;
+    else if (v == root->value) return root;
+    else if (v < root->value) return bst_search(root->left, v);
+    else return bst_search(root->right, v);
+}
+
+int main() {
+    // binary search tree de exemplo
+    node *root = new node(3);
+    node *leftChild = new node(2);
+    node *rightChild = new node(4);
+    node *leftGrandchild = new node(1);
+    node *rightGrandchild = new node(5);
+
+    root->left = leftChild;
+    root->right = rightChild;
+    leftChild->left = leftGrandchild;
+    rightChild->right = rightGrandchild;
+
+    // cálculo da altura
+    node *search = bst_search(root, 4);
+
+    cout << search->value << endl;
+
+    return 0;
+}
