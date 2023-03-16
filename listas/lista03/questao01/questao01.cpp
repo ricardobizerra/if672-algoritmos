099d7bfcf28985567560f132b7d63efdf028e9cc
Ricardo Bizerra <rblf@cin.ufpe.br>
2023-03-16 10:12:33 -0300

    feature(main): lista03/questao01 continue1

--- listas/lista03/questao01/questao01.cpp
+++ listas/lista03/questao01/questao01.cpp
@@ -3,51 +3,45 @@ using namespace std;
 
 struct node {
     int value;
-    int bf;
+    int bf; // fator de balanceamento
+    int depth; // profundidade
     node *left;
     node *right;
+    node *parent; // nó superior
 
-    node(int value): value(value), bf(0), left(NULL), right(NULL) {}
-};
-
-struct bst {
-    node *root;
-
-    bst(): root(NULL) {}
+    node(int value,  int depth = 0, node *parent = NULL): value(value), bf(0), depth(depth), left(NULL), right(NULL), parent(parent) {}
 };
 
 // find
 
-struct return_bst_find {
-    node *aux;
-    int prof;
-
-    return_bst_find(node *aux, int prof): aux(aux), prof(prof) {}
-};
-
-return_bst_find* bst_find(node *root, int v, int prof = 0) {
-    if (root == NULL) return nullptr;
-    else if (v == root->value) return new return_bst_find(root, prof);
-    else if (v < root->value) return bst_find(root->left, v, prof + 1);
-    else return bst_find(root->right, v, prof + 1);
+node* bst_find(node *root, int v) {
+    if (root == NULL) return NULL;
+    else if (v == root->value) return root;
+    else if (v < root->value) return bst_find(root->left, v);
+    else return bst_find(root->right, v);
 }
 
 // insertion
 
-node* bst_insert(node *root, int v) {
+node* bst_insert(node *root, int v, node *parent = NULL, int depth = 0) {
     if (root == NULL) {
-        node *new_node = new node(v);
+        node *new_node = new node(v, depth, parent);
         return new_node;
     }
 
-    else if (v < root->value) {
-        root->left = bst_insert(root->left, v);
-        return root;
-    }
-
     else {
-        root->right = bst_insert(root->right, v);
-        return root;
+
+        depth++;
+
+        if (v < root->value) {
+            root->left = bst_insert(root->left, v, root, depth);
+            return root;
+        }
+
+        else {
+            root->right = bst_insert(root->right, v, root, depth);
+            return root;
+        }
     }
 }
 
@@ -64,7 +58,7 @@ bst_delmin_return* bst_delete_min(node *root) {
         if (root->left == NULL) {
             int v = root->value;
             node *r = root->right;
-            delete(root);
+            free(root);
             bst_delmin_return *retorno = new bst_delmin_return(r, v);
             return retorno;
         }
@@ -78,7 +72,7 @@ bst_delmin_return* bst_delete_min(node *root) {
         }
     } 
     
-    else return nullptr;
+    else return NULL;
 }
 
 node* bst_delete(node *root, int v) {
@@ -97,13 +91,13 @@ node* bst_delete(node *root, int v) {
     else {
         if (root->left == NULL) {
             node *r = root->right;
-            delete(root);
+            free(root);
             return r;
         }
 
         else if (root->right == NULL) {
             node *r = root->left;
-            delete(root);
+            free(root);
             return r;
         }
 
@@ -124,6 +118,11 @@ node* rotate_left(node *root) {
     node *RL = root->right->left;
     R->left = root;
     root->right = RL;
+    if (root->parent != NULL) root->parent->right = R;
+
+    if (RL != NULL) RL->parent = root;
+    R->parent = root->parent;
+    root->parent = R;
 
     return R;
 }
@@ -135,35 +134,33 @@ node* rotate_right(node *root) {
     node *LR = root->left->right;
     L->right = root;
     root->left = LR;
+    if (root->parent != NULL) root->parent->left = L;
+
+    if (LR != NULL) LR->parent = root;
+    L->parent = root->parent;
+    root->parent = L;
 
     return L;
 }
 
 // splay function
 
-node* bst_find_parent(node* root, node* child) {
-    if (root == NULL or root == child) return NULL;
+node* splay(node *root, node *node_inserido, int x) {
+    node *parent_x = node_inserido->parent;
 
-    if (root->left == child or root->right == child) return root;
+    if (parent_x == NULL) return root;
 
-    if (child->value < root->value) return bst_find_parent(root->left, child);
-    else return bst_find_parent(root->right, child);
-}
-
-node* splay(node *root, node *root_auxiliar, int previous_x, int x) {
-    if (previous_x != 0) {
-        node *parent_x = bst_find(root, previous_x)->aux;
-
-        if (parent_x == NULL or root_auxiliar == NULL) return root_auxiliar;
-
-        else {
-            if (parent_x->left != NULL and parent_x->left->value == x) root_auxiliar = rotate_right(parent_x);
-            else if (parent_x->right != NULL and parent_x->right->value == x) root_auxiliar = rotate_left(parent_x);
-
-            splay(root, root_auxiliar, previous_x, x);
-        }
-        return root_auxiliar;
+    if (parent_x->left != NULL and parent_x->left->value == x) {
+        node_inserido = rotate_right(parent_x);
     }
+    else if (parent_x->right != NULL and parent_x->right->value == x) {
+        node_inserido = rotate_left(parent_x);
+    }
+    
+    if (node_inserido->left == root or node_inserido->right == root) root = node_inserido;
+    if (node_inserido == root) return root;
+
+    return splay(root, node_inserido, x);
 }
 
 // main function
@@ -180,29 +177,28 @@ int main(){
 
     while (cmd != "END") {
 
-        previous_number = number;
         cin >> number;
-        return_bst_find *retorno = bst_find(root, number);
+        node *retorno = bst_find(root, number);
 
         if (cmd == "INS") {
-            if (retorno != NULL) cout << retorno->prof << endl;
+            if (retorno != NULL) cout << retorno->depth << endl;
             else {
                 if (root == NULL) root = new node(number);
                 else bst_insert(root, number);
-                cout << bst_find(root, number)->prof << endl;
-                root = splay(root, root, previous_number, number);
+                cout << bst_find(root, number)->depth << endl;
+                root = splay(root, bst_find(root, number), number);
             }
         } 
         
         else {
-            cout << retorno->prof << endl;
+            cout << retorno->depth << endl;
 
-            if (cmd == "FND") splay(root, root, previous_number, number);
+            if (cmd == "FND") splay(root, bst_find(root, number), number);
 
             else if (cmd == "DEL") {
-                node *parent_deleted = bst_find_parent(root, retorno->aux);
-                bst_delete(retorno->aux, number);
-                splay(root, root, previous_number, parent_deleted->value);
+                node *parent_deleted = retorno->parent;
+                bst_delete(retorno, number);
+                splay(root, bst_find(root, parent_deleted->value), parent_deleted->value);
             }
         }
 
